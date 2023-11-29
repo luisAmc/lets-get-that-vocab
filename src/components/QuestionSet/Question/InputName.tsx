5f5852a69d8f0a2ac7c1a78ac50695f6714a6910
luisAmc <luis.kosm@gmail.com>
2023-11-28 19:05:31 -0600

    Updates `InputName` to new design

--- src/components/QuestionSet/Question/InputName.tsx
+++ src/components/QuestionSet/Question/InputName.tsx
@@ -3,149 +3,165 @@ import { useQuestionSet } from '../QuestionSetContext';
 import { cn } from '../../../utils/cn';
 import { AnimatePresence, motion } from 'framer-motion';
 import {
-    ArrowRightIcon,
-    CheckIcon,
-    XMarkIcon,
+	ArrowRightIcon,
+	CheckIcon,
+	XMarkIcon,
 } from '@heroicons/react/24/outline';
+import { Pill } from '~/components/shared/Pill';
+import { Button } from '~/components/shared/Button';
+import { Form, useZodForm } from '~/components/shared/Form';
+import { z } from 'zod';
+import { useWatch } from 'react-hook-form';
+
+const inputNameSchema = z.object({
+	input: z.string().min(1, 'Ingrese el nombre'),
+});
 
 export function InputName() {
-    const {
-        questionCount,
-        answerCount,
-        getCurrentQuestion,
-        answerQuestion,
-        goToNextQuestion,
-    } = useQuestionSet();
+	const {
+		questionCount,
+		answerCount,
+		getCurrentQuestion,
+		answerQuestion,
+		goToNextQuestion,
+	} = useQuestionSet();
 
-    const question = getCurrentQuestion();
+	const question = getCurrentQuestion();
 
-    const [nameEntered, setNameEntered] = useState<string>('');
-    const [showAnswer, setShowAnswer] = useState(false);
+	const form = useZodForm({ schema: inputNameSchema });
 
-    function handleVerifyClick() {
-        setShowAnswer(true);
-    }
+	const nameEntered = useWatch({ control: form.control, name: 'input' });
 
-    function handleContinueClick() {
-        answerQuestion(nameEntered.trim() === question.answer);
+	const [showAnswer, setShowAnswer] = useState(false);
 
-        setNameEntered('');
-        setShowAnswer(false);
+	function handleVerifyClick() {
+		console.log({ nameEntered });
+		setShowAnswer(true);
+	}
 
-        if (answerCount < questionCount) {
-            goToNextQuestion();
-        }
-    }
+	function handleContinueClick() {
+		answerQuestion(nameEntered.trim() === question.answer);
 
-    const isAnswer = nameEntered === question.answer;
+		setShowAnswer(false);
 
-    return (
-        <section className="flex flex-1 flex-col gap-y-4">
-            <h1 className="text-center text-3xl">Escribe el nombre</h1>
+		if (answerCount < questionCount) {
+			goToNextQuestion();
+		}
+	}
 
-            <div className="relative overflow-hidden rounded-xl">
-                <img
-                    className="aspect-[1/1] w-full object-cover"
-                    src={question.word.imgSrc}
-                />
+	const isAnswer = nameEntered === question.answer;
 
-                <AnimatePresence>
-                    {showAnswer && (
-                        <motion.div
-                            initial="initial"
-                            animate="animate"
-                            exit="exit"
-                            variants={{
-                                initial: { opacity: 0 },
-                                animate: { opacity: 1 },
-                                exit: { opacity: 0 },
-                            }}
-                            className={cn(
-                                'absolute inset-0 flex flex-col items-center justify-center bg-transparent',
-                                isAnswer
-                                    ? 'bg-green-300/80 text-green-700'
-                                    : 'bg-rose-300/80 text-rose-700'
-                            )}
-                        >
-                            {isAnswer ? (
-                                <CheckIcon className="h-72 w-72" />
-                            ) : (
-                                <XMarkIcon className="h-72 w-72" />
-                            )}
+	return (
+		<section className="flex h-full flex-1 flex-col gap-y-4 pt-2">
+			<div className="flex items-center justify-between">
+				<h1 className="text-lg font-medium">Escribe el nombre</h1>
 
-                            <span className="text-5xl font-bold">
-                                {question.answer}
-                            </span>
-                        </motion.div>
-                    )}
-                </AnimatePresence>
-            </div>
+				<Pill>{question.word.tag.name}</Pill>
+			</div>
 
-            <input
-                className={cn(
-                    'h-14 w-full rounded-xl border-2 border-solid border-transparent px-4 py-2 text-center text-xl text-gray-900 ring-2',
-                    'focus:border-yellow-primary focus:outline-none focus:ring-yellow-primary',
-                    showAnswer &&
-                        (isAnswer
-                            ? 'bg-green-300 text-green-700'
-                            : 'bg-rose-300 text-rose-700')
-                )}
-                value={nameEntered}
-                onChange={(e) => setNameEntered(e.target.value)}
-                placeholder="Escribe aquí..."
-            />
+			<div className="relative overflow-hidden rounded-xl">
+				<div className="aspect-square w-full bg-gray-300">
+					<img
+						className="aspect-square w-full object-cover"
+						src={question.word.imgSrc}
+					/>
+				</div>
 
-            {!showAnswer && (
-                <VerifyButton
-                    isOptionSelected={Boolean(nameEntered)}
-                    onClick={handleVerifyClick}
-                />
-            )}
+				<AnimatePresence>
+					{showAnswer && (
+						<motion.div
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							variants={{
+								initial: { opacity: 0 },
+								animate: { opacity: 1 },
+								exit: { opacity: 0 },
+							}}
+							className={cn(
+								'absolute inset-0 flex flex-col items-center justify-center bg-transparent',
+								isAnswer
+									? 'bg-green-300/80 text-green-700'
+									: 'bg-rose-300/80 text-rose-700',
+							)}
+						>
+							{isAnswer ? (
+								<CheckIcon className="h-72 w-72" />
+							) : (
+								<XMarkIcon className="h-72 w-72" />
+							)}
 
-            {showAnswer && <ContinueButton onClick={handleContinueClick} />}
-        </section>
-    );
+							<span className="text-5xl font-bold">{question.answer}</span>
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</div>
+
+			<div className="flex-1"></div>
+
+			<div>
+				<Form form={form} onSubmit={handleVerifyClick}>
+					<input
+						{...form.register('input')}
+						className={cn(
+							'h-14 w-full rounded-xl border-2 border-solid border-transparent px-4 py-2 text-center text-xl text-gray-900 ring-2',
+							'focus:border-yellow-primary focus:ring-yellow-primary focus:outline-none',
+							showAnswer &&
+								(isAnswer
+									? 'bg-green-300 text-green-700'
+									: 'bg-rose-300 text-rose-700'),
+						)}
+						placeholder="Escribe aquí..."
+					/>
+
+					{!showAnswer && (
+						<VerifyButton
+							isOptionSelected={Boolean(nameEntered)}
+							onClick={handleVerifyClick}
+						/>
+					)}
+
+					{showAnswer && <ContinueButton onClick={handleContinueClick} />}
+				</Form>
+			</div>
+		</section>
+	);
 }
 
 interface ContinueButtonProps {
-    onClick(): void;
+	onClick(): void;
 }
 
 function ContinueButton({ onClick }: ContinueButtonProps) {
-    return (
-        <button
-            type="button"
-            className={cn(
-                'flex h-14 items-center justify-between gap-x-2 rounded-xl bg-gray-100 px-4 py-3 text-lg font-medium text-gray-500 transition-opacity ease-in hover:opacity-75'
-            )}
-            onClick={onClick}
-        >
-            <span>Siguiente</span>
-            <ArrowRightIcon className="animate-bounce-horizontal h-6 w-6" />
-        </button>
-    );
+	return (
+		<Button
+			size="xl"
+			variant="secondary"
+			className="flex items-center justify-between"
+			onClick={onClick}
+		>
+			<span>Siguiente</span>
+			<ArrowRightIcon className="animate-bounce-horizontal h-6 w-6" />
+		</Button>
+	);
 }
 
 interface VerifyButtonProps {
-    isOptionSelected: boolean;
-    onClick(): void;
+	isOptionSelected: boolean;
+	onClick(): void;
 }
 
 function VerifyButton({ isOptionSelected, onClick }: VerifyButtonProps) {
-    return (
-        <button
-            type="button"
-            onClick={onClick}
-            disabled={!isOptionSelected}
-            className={cn(
-                'h-14 w-full rounded-xl bg-gray-300 px-4 py-3 text-xl font-medium text-gray-400 shadow-sm transition-opacity hover:opacity-80',
-                !isOptionSelected && 'cursor-not-allowed hover:opacity-100',
-                isOptionSelected && 'bg-green-200 text-green-600'
-            )}
-        >
-            <span>
-                {isOptionSelected ? 'Verificar' : 'Seleccione una opción'}
-            </span>
-        </button>
-    );
+	return (
+		<>
+			<Button
+				size="xl"
+				variant={isOptionSelected ? 'positive' : 'secondary'}
+				onClick={onClick}
+				disabled={!isOptionSelected}
+			>
+				<span>{isOptionSelected ? 'Verificar' : 'Seleccione una opción'}</span>
+			</Button>
+		</>
+	);
 }

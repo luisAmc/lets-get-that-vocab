f253b6f6c6e5f2d6ded776973e66938a41cc4103
luisAmc <luis.kosm@gmail.com>
2023-11-29 10:34:56 -0600

    Fix spacing in the home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -64,63 +64,65 @@ export function Home() {
 				<>
 					{/* <Header /> */}
 
-					{data
-						.filter((unit) => unit.lessons.length > 0)
-						.map((unit, unitIndex) => (
-							<section
-								key={unit.id}
-								className="flex w-full flex-col rounded-xl bg-white"
-							>
-								<div
-									className={cn(
-										['bg-green-400', 'bg-purple-400', 'bg-teal-400'][
-											unitIndex % 3
-										],
-										'flex items-center rounded-xl p-6',
-									)}
+					<div className='flex flex-col gap-y-4'>
+						{data
+							.filter((unit) => unit.lessons.length > 0)
+							.map((unit, unitIndex) => (
+								<section
+									key={unit.id}
+									className="flex w-full flex-col rounded-xl bg-white"
 								>
-									<span className="text-3xl font-medium text-white">
-										{unit.name}
-									</span>
-								</div>
-
-								<AnimatePresence>
-									<div className="flex flex-col items-center justify-center gap-y-4 py-4">
-										{unit.lessons
-											.filter((lesson) => lesson._count.words > 0)
-											.map((lesson, lessonIndex) => (
-												<motion.div
-													key={lesson.id}
-													className={
-														unitIndex % 2 === 0
-															? shiftValues[lessonIndex % shiftValues.length]
-															: shiftValuesReversed[
-																	lessonIndex % shiftValues.length
-															  ]
-													}
-													initial="initial"
-													animate="animate"
-													variants={lessonVariants}
-													custom={lessonIndex}
-												>
-													<button
-														onClick={() => handleLessonClick(lesson)}
-														className={cn(
-															'relative flex items-center justify-center rounded-full border-b-8 border-green-800 bg-green-500 px-6 py-4 shadow-md',
-															'hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-yellow-500 focus:ring-offset-4',
-															'transition-transform hover:-translate-y-0.5',
-														)}
-													>
-														<span className="text-lg font-semibold text-white">
-															{lesson.name}
-														</span>
-													</button>
-												</motion.div>
-											))}
+									<div
+										className={cn(
+											['bg-green-400', 'bg-purple-400', 'bg-teal-400'][
+												unitIndex % 3
+											],
+											'flex items-center rounded-xl p-6',
+										)}
+									>
+										<span className="text-3xl font-medium text-white">
+											{unit.name}
+										</span>
 									</div>
-								</AnimatePresence>
-							</section>
-						))}
+
+									<AnimatePresence>
+										<div className="flex flex-col items-center justify-center gap-y-4 py-4">
+											{unit.lessons
+												.filter((lesson) => lesson._count.words > 0)
+												.map((lesson, lessonIndex) => (
+													<motion.div
+														key={lesson.id}
+														className={
+															unitIndex % 2 === 0
+																? shiftValues[lessonIndex % shiftValues.length]
+																: shiftValuesReversed[
+																		lessonIndex % shiftValues.length
+																  ]
+														}
+														initial="initial"
+														animate="animate"
+														variants={lessonVariants}
+														custom={lessonIndex}
+													>
+														<button
+															onClick={() => handleLessonClick(lesson)}
+															className={cn(
+																'relative flex items-center justify-center rounded-full border-b-8 border-green-800 bg-green-500 px-6 py-4 shadow-md',
+																'hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-yellow-500 focus:ring-offset-4',
+																'transition-transform hover:-translate-y-0.5',
+															)}
+														>
+															<span className="text-lg font-semibold text-white">
+																{lesson.name}
+															</span>
+														</button>
+													</motion.div>
+												))}
+										</div>
+									</AnimatePresence>
+								</section>
+							))}
+					</div>
 
 					<div className="flex-1"></div>
 				</>

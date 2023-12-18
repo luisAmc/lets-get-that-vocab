735aa749cb1e400fc5af287a65a01f814edb1f56
luisAmc <luis.kosm@gmail.com>
2023-12-17 23:25:41 -0600

    Uses margin-bottom instead of flex-box in page

--- src/components/shared/Page.tsx
+++ src/components/shared/Page.tsx
@@ -28,29 +28,27 @@ export function Page({
 
 	return (
 		<main className="relative mx-auto h-full w-full max-w-lg px-2 py-4">
-			<div className="flex flex-col gap-y-4">
-				{hasHeader && (
-					<header className="flex items-center justify-between">
-						<div className="flex items-center gap-x-4">
-							{(goBack || to) && (
-								<Button
-									variant="secondary"
-									size="icon"
-									href={goBack ? backRoute : to}
-								>
-									<ChevronLeftIcon className="h-5 w-5" />
-								</Button>
-							)}
+			{hasHeader && (
+				<header className="mb-4 flex items-center justify-between">
+					<div className="flex items-center gap-x-4">
+						{(goBack || to) && (
+							<Button
+								variant="secondary"
+								size="icon"
+								href={goBack ? backRoute : to}
+							>
+								<ChevronLeftIcon className="h-5 w-5" />
+							</Button>
+						)}
 
-							{title && <h1 className="text-2xl">{title}</h1>}
-						</div>
+						{title && <h1 className="text-2xl">{title}</h1>}
+					</div>
 
-						{action}
-					</header>
-				)}
+					{action}
+				</header>
+			)}
 
-				{children}
-			</div>
+			{children}
 
 			<PrivacyScreen />
 		</main>

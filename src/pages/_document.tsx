67a685e89e317db1ac9e931cc134d584957933fb
luisAmc <luis.kosm@gmail.com>
2024-03-18 22:23:17 -0600

    Updates pwa's theme color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
 			<Head />
 			<link rel="manifest" href="/manifest.json" />
 			<link rel="apple-touch-icon" href="/icon-512x512.png" />
-			<meta name="theme-color" content="#438e96" />
+			<meta name="theme-color" content="#ffffff" />
 
 			<body>
 				<Main />
